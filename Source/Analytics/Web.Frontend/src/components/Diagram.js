e0f65697d1363756b5098e1fae1680d683a4a353
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-06-11 16:10:33 +0200

    Material-ify epicurve diagram

--- Source/Analytics/Web.Frontend/src/components/Diagram.js
+++ Source/Analytics/Web.Frontend/src/components/Diagram.js
@@ -5,6 +5,10 @@ import { SegmentedControl, Alert, Button } from "evergreen-ui";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { getJson } from "../utils/request";
+import Paper from '@material-ui/core/Paper';
+import Switch from '@material-ui/core/Switch';
+import Typography from '@material-ui/core/Typography';
+
 
 import { BASE_URL } from "./Analytics";
 
@@ -22,7 +26,7 @@ const defaultOptions = {
     yAxis: {
         min: 0,
         title: {
-            text: "Number in cases in total"
+            text: "Number of cases in total"
         }
     },
     plotOptions: {
@@ -92,13 +96,33 @@ class Diagram extends Component {
         }
     }
 
+    handleChange = () => {
+        if (value === 'Week'){
+            this.setState({value: 'Day'})
+        } else {
+            this.setState({value: 'Week'})
+        }
+    }
+
     render() {
         var options = {
             ...defaultOptions,
             title: { text: this.props.title(this.state.value) },
             series: this.state.series,
-            xAxis: this.state.xAxis
+            xAxis: this.state.xAxis,
+            chart: {
+                style: {
+                    fontFamily: 'Roboto'
+                }
+            },
+            tooltip: { fontFamily: 'Roboto'},
+            title: {
+                align: 'left',
+                variant: 'h6'
+            }
         };
+
+        
 
         if (this.state.isError) {
             return (
@@ -106,38 +130,42 @@ class Diagram extends Component {
                     className="analytics--loadingContainer"
                     style={{ height: "450px" }}
                 >
-                    <Alert
-                        intent="danger"
-                        title="We could not the reach the backend"
-                    >
-                        {`Url: ${this.url}`}
-                    </Alert>
-                    <Button
-                        marginTop={"20px"}
-                        iconBefore="repeat"
-                        onClick={() => this.fetchData()}
-                    >
-                        Retry
-                    </Button>
+                    <Paper>
+                        <div style={{display: "flex", padding: 5}}>
+                            <div style={{padding: 10}}><i class="fa fa-exclamation-triangle"></i></div>
+                            <div>
+                                <Typography variant="h6" style={{color: "#9f0000"}}>We could not the reach the backend.</Typography>
+                                <Typography>{`Url: ${this.url}`}</Typography>
+                                <Button
+                                    variant="contained"
+                                    onClick={() => this.fetchData()}
+                                    style={{marginTop: 10, marginBottom: 10}}
+                                >
+                                    Retry
+                                </Button>
+                            </div>     
+                        </div>      
+                    </Paper>
                 </div>
             );
         }
 
         return (
             <>
-                {this.props.hasDatePicker && (
-                    <div className="analytics--diagramLabel">
-                        <SegmentedControl
-                            name="switch"
-                            width={250}
-                            height={30}
-                            options={this.state.options}
+                <Paper>
+                    <div style={{padding: 10}}>
+                    <div style={{display: "flex", alignItems: "center"}}>
+                        <Typography>Day</Typography>
+                        <Switch 
                             value={this.state.value}
-                            onChange={value => this.setState({ value })}
+                            onChange={value => this.setState(this.state.value === 'Day' ? { value: 'Week' } : { value : 'Day' })}
+                            options={this.state.options}
                         />
+                        <Typography>Week</Typography>
                     </div>
-                )}
-                <HighchartsReact highcharts={Highcharts} options={options} />
+                    <HighchartsReact highcharts={Highcharts} options={options} />
+                    </div>
+                </Paper>
             </>
         );
     }
