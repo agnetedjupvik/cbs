0543f326dfe5cd8162cb6a773e7216d0960465b0
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-06-11 16:10:00 +0200

    Create headline function, remove unused code

--- Source/Analytics/Web.Frontend/src/components/Analytics.js
+++ Source/Analytics/Web.Frontend/src/components/Analytics.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Map from "./Map.js";
 import Diagram from "./Diagram.js";
 import Typography from '@material-ui/core/Typography';
 
@@ -15,21 +14,27 @@ class Analytics extends Component {
     constructor(props) {
         super(props);
     }
+
+    headline(text, variant) {
+        return(
+        <Typography variant={variant} gutterBottom style={{marginTop: 40}}>
+            {text}
+        </Typography>
+        );
+    };
     render() {
+        
         return (
             <div className="analytics--container">
                 <CBSNavigation />
-                <Typography component="h1" variant="headline" gutterBottom>
-                    Country Overview
-                </Typography>
+                {this.headline("Country Overview", "h2")}
 
+                {this.headline("Project Presence", "h4")}
                 <ProjectPresence />
 
+                {this.headline("Situation Report", "h4")}
                 <CaseReportByHealthRiskTable />
                 <HealthRiskPerDistrictTable />
-                
-                <Map />
-                
 
                 <Diagram
                     selectedSeries={["Total"]}
@@ -37,50 +42,6 @@ class Analytics extends Component {
                     defaultRange={"Day"}
                     title={range => `Epicurve by ${range}`}
                 />
-                {/*
-                <Diagram
-                    selectedSeries={["AgeUnderFive", "AgeFiveOrAbove"]}
-                    hasDatePicker
-                    defaultRange={"Week"}
-                    title={range => `Epicurve by ${range} Dodged by Age`}
-                />
-                <div className={"analytics--twoDiagrams"}>
-                    <div className="analytics--insideDiagram">
-                        <Diagram
-                            selectedSeries={["MaleUnderFive"]}
-                            hasDatePicker={false}
-                            defaultRange={"Week"}
-                            title={_ => `Male Under five`}
-                        />
-                    </div>
-                    <div className="analytics--insideDiagram">
-                        <Diagram
-                            selectedSeries={["MaleFiveOrAbove"]}
-                            hasDatePicker={false}
-                            defaultRange={"Week"}
-                            title={_ => `Male Five or Above`}
-                        />
-                    </div>
-                </div>
-                <div className={"analytics--twoDiagrams"}>
-                    <div className="analytics--insideDiagram">
-                        <Diagram
-                            selectedSeries={["FemaleUnderFive"]}
-                            hasDatePicker={false}
-                            defaultRange={"Week"}
-                            title={_ => `Female Under five`}
-                        />
-                    </div>
-                    <div className="analytics--insideDiagram">
-                        <Diagram
-                            selectedSeries={["FemaleFiveOrAbove"]}
-                            hasDatePicker={false}
-                            defaultRange={"Week"}
-                            title={_ => `Female Five or Above`}
-                        />
-                    </div>
-                </div>
-                */}
             </div>
         );
     }
